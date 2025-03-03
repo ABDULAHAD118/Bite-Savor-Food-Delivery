ff779910eebc0634b7a46667b806b90b581db0b0
ABDULAHAD118 <129290085+ABDULAHAD118@users.noreply.github.com>
2025-03-03 15:39:26 +0500

    Complete the order and cart section and finish the frontend

--- Frontend/src/pages/Order/PlaceOrder.tsx
+++ Frontend/src/pages/Order/PlaceOrder.tsx
@@ -1,10 +1,48 @@
+import { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../contexts/StoreContext'
 
 const PlaceOrder = () => {
+    const { getTotalCartAmount } = useContext<any>(StoreContext);
     return (
         <div className='place-order'>
-            <div className="place-order-left"></div>
-            <div className="place-order-right"></div>
+            <div className="place-order-left">
+                <div className="title">Delivery Information</div>
+                <div className="multi-fields">
+                    <input type="text" placeholder='First Name' />
+                    <input type="text" placeholder='Last Name' />
+                </div>
+                <input type="email" placeholder='Email Address' />
+                <input type="text" placeholder='Street' />
+                <div className="multi-fields">
+                    <input type="text" placeholder='Zip Code' />
+                    <input type="text" placeholder='Country' />
+                </div>
+                <input type="number" placeholder='Phone' />
+            </div>
+            <div className="place-order-right">
+                <div className="cart-total">
+                    <h2 >Cart Totals</h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>Subtotal</p>
+                            <p>${getTotalCartAmount()}</p>
+                        </div>
+                        <hr />
+
+                        <div className="cart-total-details">
+                            <p>Delivery Fee</p>
+                            <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Total</p>
+                            <p>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</p>
+                        </div>
+                    </div>
+                    <button>Proceed to Payment</button>
+                </div>
+            </div>
         </div>
     )
 }
